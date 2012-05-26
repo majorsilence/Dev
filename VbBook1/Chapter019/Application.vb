a1f65112faac4c79d36d71d07f4c004d0837d6ed
Peter Gill <peter@majorsilence.com>
2012-05-26 15:01:32 -0230

    NUnit code.

---
+++ VbBook1/Chapter019/Application.vb
@@ -0,0 +1,14 @@
+﻿
+
+Module Application
+
+
+    Public Sub Main()
+        ' This is an empty function that is not needed.
+        ' The tests are in the class TestingClass.vb
+
+
+
+    End Sub
+
+End Module
