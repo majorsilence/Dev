d472cf70b9d5d390f991399f886de6129eafc53b
Peter Gill <peter@majorsilence.com>
2013-03-18 20:51:22 -0230

    Add subtraction example

--- VbBook1/Calculations/Application.vb
+++ VbBook1/Calculations/Application.vb
@@ -8,12 +8,15 @@ Public Class Application
 		System.Console.WriteLine("A is: " & a.ToString)
 		
 		a = (a + 2) ' Addition
-		System.Console.WriteLine("A + 2 is: " & a.ToString)
+        System.Console.WriteLine("A + 2 is: " & a.ToString)
+
+        a = (a - 1) ' Subtraction
+        System.Console.WriteLine("A - 1 is: " & a.ToString)
 		
 		a = (a * 3) ' Multiplication
 		System.Console.WriteLine("A * 3 is: " & a.ToString)
 		
-		a = cint(a / 2) ' Division
+        a = CInt(a / 2) ' Division
 		System.Console.WriteLine("A / 2 is: " & a.ToString)
 		
 		System.Console.WriteLine("")
@@ -24,7 +27,10 @@ Public Class Application
 		b = (b + 2) ' Addition
 		System.Console.WriteLine("B + 2 is: " & b.ToString)
 		b = (b + 1.65432D) ' Addition
-		System.Console.WriteLine("B + 1.65432 is: " & b.ToString)
+        System.Console.WriteLine("B + 1.65432 is: " & b.ToString)
+
+        b = (b + 1.65432D) ' Subtraction
+        System.Console.WriteLine("B - 2.2 is: " & b.ToString)
 		
 		b = (b * 3.01D) ' Multiplication
 		System.Console.WriteLine("B * 3.01 is: " & b.ToString)
