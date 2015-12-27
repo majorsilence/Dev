9a0bf5c79f31350ed2cf404ef5cfe7f6eba32dbc
Peter Gill <peter@majorsilence.com>
2015-12-27 12:46:18 -0330

    c# version of calculations

---
+++ CSharp/calcs.cs
@@ -0,0 +1,41 @@
+
+
+System.Console.WriteLine ("This chapter will teach basic math.");
+
+var a = 2;
+System.Console.WriteLine ("A is: " + a.ToString());
+
+a = (a + 2); // Addition
+System.Console.WriteLine("A + 2 is: " + a.ToString());
+
+a = (a - 1); // Subtraction
+System.Console.WriteLine("A - 1 is: " + a.ToString());
+
+a = (a * 3); // Multiplication
+System.Console.WriteLine("A * 3 is: " + a.ToString());
+
+a = a / 2; // Division
+System.Console.WriteLine("A / 2 is: " + a.ToString());
+
+System.Console.WriteLine ("");
+				
+var b = 2.22m;
+System.Console.WriteLine ("B is: " + b.ToString());
+
+b = (b + 2); // Addition
+System.Console.WriteLine("B + 2 is: " + b.ToString());
+b = (b + 1.65432m); // Addition
+System.Console.WriteLine("B + 1.65432 is: " + b.ToString());
+
+b = (b + 1.65432m); // Subtraction
+System.Console.WriteLine("B - 2.2 is: " + b.ToString());
+
+b = (b * 3.01m); // Multiplication
+System.Console.WriteLine("B * 3.01 is: " + b.ToString());
+
+b = (b / 2.4m); // Division
+System.Console.WriteLine("B / 2.4 is: " + b.ToString())	;
+
+System.Console.WriteLine("");
+System.Console.WriteLine("You can also of course do math without assigning the results to variables");
+System.Console.WriteLine(2+2);
