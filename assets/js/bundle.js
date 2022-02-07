4a3c736cd9eaed4c1dc3cb81015128e31886b3ee
Peter Gill <peter@majorsilence.com>
2022-02-06 21:49:04 -0330

    Upgrade theme
    https://html5up.net/editorial
    Creative Commons Attribution 3.0 Unported

---
+++ assets/js/bundle.js
@@ -0,0 +1,8 @@
+---
+---
+{% include /assets/js/jquery.min.js %}
+{% include /assets/js/browser.min.js %}
+{% include /assets/js/breakpoint.min.js %}
+{% include /assets/js/util.js %}
+{% include /assets/js/main.js %}
+{% include /assets/js/menu.js %}
