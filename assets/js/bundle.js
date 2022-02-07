c620746193530f7bf4697d360752a203438f3f6b
Peter Gill <peter@majorsilence.com>
2022-02-06 21:57:07 -0330

    fix type

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -2,7 +2,7 @@
 ---
 {% include_relative jquery.min.js %}
 {% include_relative browser.min.js %}
-{% include_relative breakpoint.min.js %}
+{% include_relative breakpoints.min.js %}
 {% include_relative util.js %}
 {% include_relative main.js %}
 {% include_relative menu.js %}
