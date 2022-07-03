0486c47367b68646ad525a0175ebbc962681826d
Peter Gill <peter.gill@townsuite.com>
2022-07-02 22:59:09 -0230

    update bundles

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -3,6 +3,5 @@
 {% include_relative jquery.min.js %}
 {% include_relative browser.min.js %}
 {% include_relative breakpoints.min.js %}
-{% include_relative util.js %}
 {% include_relative main.js %}
 {% include_relative menu.js %}
