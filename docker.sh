b5ddc6f3752e5b5fd04fe9ca9cff850080fd0388
Peter Gill <peter.gill@townsuite.com>
2022-07-16 11:34:53 -0230

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@
 set -e # exit on first error
 set -u # exit on using unset variable
 
-sudo docker run -v "$(pwd)":/src -p 4000:4000 -it jekyll/jekyll:stable bash -c "mkdir -p /src && cd /src && jekyll serve --incremental"
+sudo docker run -v "$(pwd)":/src -p 4000:4000 -it jekyll/jekyll:4.2.0 bash -c "mkdir -p /src && cd /src && jekyll serve --incremental"
 
