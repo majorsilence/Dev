9bf219dac5d90376dffc710d8ad6b2a47b470a06
Peter Gill <peter.gill@townsuite.com>
2022-07-16 15:35:56 -0230

    remove unused javascript

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,7 +1,5 @@
 ---
 ---
-{% include_relative jquery.min.js %}
-{% include_relative browser.min.js %}
-{% include_relative breakpoints.min.js %}
+
 {% include_relative main.js %}
-{% include_relative menu.js %}
+
